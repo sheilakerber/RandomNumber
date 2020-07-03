13652a78cca20af38bcfa45cf87da899181a778d
Sheila Kerber <sheilask.bio@gmail.com>
2020-07-03 10:08:08 -0300

    simplifica o codigo com a reutilizacao de funcoes

--- script.js
+++ script.js
@@ -1,85 +1,67 @@
-function random09() {
+function randomNumber(multiplicador, inputUsuario) {
+    let numAleatorioAutomatico = -1
+    let numExecucoes = 0
+    while (numAleatorioAutomatico != inputUsuario) {
+        numAleatorioAutomatico = Math.floor(Math.random() * multiplicador)
+        numExecucoes += 1
+        console.log("Numeros gerados automaticamente: ", numAleatorioAutomatico)
+        console.log("Numero execucoes: ", numExecucoes)
+    }
+    return numExecucoes
+}
 
+function random09(num) {
     let num09 = document
         .getElementById("num0ate9")
         .value
 
+    let input09 = document.getElementById("res0ate9")
+
     if (num09 < 0 || num09 > 9) {
         window.alert("O número deve estar entre 0 e 9!")
-        document
-            .getElementById("num0ate9")
-            .value = ''
+        limpaInput09(num09)
     } else {
-
-        let numAleatorioAutomatico = -1
-        let numExecucoes = 0
-
-        while (numAleatorioAutomatico != num09) {
-            numAleatorioAutomatico = Math.floor(Math.random() * 10)
-            numExecucoes += 1
-            console.log("Numeros gerados automaticamente: ", numAleatorioAutomatico)
-            console.log("Numero execucoes: ", numExecucoes)
-        }
-
+        let numExecucoes = randomNumber(10, num09)
         if (numExecucoes == 1) {
-            document
-                .getElementById("res0ate9")
-                .innerHTML = `A quantidade de execuções para o computador gerar o número ${num09} foi de ${numExecucoes} vez!`
-
-            document
-                .getElementById("num0ate9")
-                .value = '';
+            input09.innerHTML = `A quantidade de execuções para o computador gerar o número ${num09} foi de ${numExecucoes} vez!`
+            limpaInput09(num09)
+        } else {
+            input09.innerHTML = `A quantidade de execuções para o computador gerar o número ${num09} foi de ${numExecucoes} vezes!`
+            limpaInput09(num09)
         }
-
-        document
-            .getElementById("res0ate9")
-            .innerHTML = `A quantidade de execuções para o computador gerar o número ${num09} foi de ${numExecucoes} vezes!`
-
-        document
-            .getElementById("num0ate9")
-            .value = '';
     }
 }
 
-function random99() {
+function limpaInput09(inputUsuario) {
+    document
+        .getElementById("num0ate9")
+        .value = ''
+}
 
+function random99() {
     let num99 = document
         .getElementById("num0ate99")
         .value
 
+    let input99 = document.getElementById("res0ate99")
+
     if (num99 < 0 || num99 > 99) {
         window.alert("O número deve estar entre 0 e 99!")
-        document
-            .getElementById("num0ate99")
-            .value = ''
+        limpaInput99(num99)
     } else {
-
-        let numAleatorioAutomatico = -1
-        let numExecucoes = 0
-
-        while (numAleatorioAutomatico != num99) {
-            numAleatorioAutomatico = Math.floor(Math.random() * 100)
-            numExecucoes += 1
-            console.log("Numeros gerados automaticamente: ", numAleatorioAutomatico)
-            console.log("Numero execucoes: ", numExecucoes)
-        }
-
+        let numExecucoes = randomNumber(100, num99)
         if (numExecucoes == 1) {
-            document
-                .getElementById("res0ate99")
-                .innerHTML = `A quantidade de execuções para o computador gerar o número ${num99} foi de ${numExecucoes} vez!`
-
-            document
-                .getElementById("num0ate99")
-                .value = '';
+            input99.innerHTML = `A quantidade de execuções para o computador gerar o número ${num99} foi de ${numExecucoes} vez!`
+            limpaInput99(num99)
+        } else {
+            input99.innerHTML = `A quantidade de execuções para o computador gerar o número ${num99} foi de ${numExecucoes} vezes!`
+            limpaInput99(num99)
         }
-
-        document
-            .getElementById("res0ate99")
-            .innerHTML = `A quantidade de execuções para o computador gerar o número ${num99} foi de ${numExecucoes} vezes!`
-
-        document
-            .getElementById("num0ate99")
-            .value = '';
     }
+}
+
+function limpaInput99(inputUsuario) {
+    document
+        .getElementById("num0ate99")
+        .value = ''
 }
